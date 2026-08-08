40ec812a6420cb0de38d1de2321feb74a4df0f60
klhart88 <klhart88@gmail.com>
2026-08-07 22:29:32 -0400

    Update Supabase URL in supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -10,7 +10,7 @@
 // values from Project Settings > API.
 // ============================================
 
-const SUPABASE_URL = 'https://hadlrfksquraiopbqwjw.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://hadlrfksquraiopbqwjw.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhZGxyZmtzcXVyYWlvcGJxd2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMjg1NDksImV4cCI6MjEwMTcwNDU0OX0.eUIxe8i7JwBRiBXAVtiuaqFwa__l4a270vygHP-gbQY';
 
 export const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
